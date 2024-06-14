41137729dbee79fd1184ed78b371c7b6309a378b
Exokem <ex010@live.com>
2024-06-14 12:14:01 -0400

    Update for 1.5, restore 1.3 and 1.4 assemblies, keep redundant assemblies removed

---
+++ Source/1.5/Dialogs.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using RimWorld;
+using Verse.Sound;
+
+using Verse;
+
+namespace AlternateArchonexus
+{
+    public class Dialog_ChooseSoldSettlement : Window
+    {
+        private Action<Map> postSelected;
+        private Action cancel;
+
+        private Map selectedMap;
+
+        private Vector2 scrollPosition;
+
+        public bool isFinal = false;
+
+        private AcceptanceReport AcceptanceReport 
+        {
+            get 
+            {
+                if (selectedMap == null)
+                    return AcceptanceReport.WasRejected;
+                
+                else if (!ValidateSelection())
+                    return $"Insufficient wealth in selected settlement: {selectedMap.wealthWatcher.WealthTotal}";
+                
+                else return AcceptanceReport.WasAccepted;
+            }
+        }
+
+        public Dialog_ChooseSoldSettlement(Action<Map> postSelected, Action cancel = null)
+        {
+            if (ModLister.CheckIdeology("Choose new colony"))
+            {
+                this.postSelected = postSelected;
+                this.cancel = cancel;
+
+                forcePause = true;
+                closeOnCancel = isFinal;
+                absorbInputAroundWindow = true;
+                forceCatchAcceptAndCancelEventEvenIfUnfocused = true;
+                preventSave = true;
+            }
+        }
+
+        public override void PostOpen()
+        {
+            selectedMap = null;
+        }
+
+        public override void DoWindowContents(Rect rect)
+        {
+            float w = rect.width;
+
+            Text.Font = GameFont.Medium;
+            Widgets.Label(new Rect(0F, 0F, w, 40F), "Settlement Selection");
+            Text.Font = GameFont.Small;
+            Widgets.Label(new Rect(0F, 40F, w, 25F), "Designate which settlement should be sold.");
+            
+            List<Map> playerMaps = new List<Map>();
+
+            foreach (Map map in Find.Maps)
+            {
+                if (map.IsPlayerHome)
+                    playerMaps.Add(map);
+            }
+
+            Rect outRect = new Rect(rect.x, rect.y + 40F + 25F + 10F, w, rect.height - 40F - 25F - 70F);
+            float scrollHeight = 40F + (float) playerMaps.Count * 40F;
+
+            if (outRect.height < scrollHeight)
+                scrollHeight -= 16F;
+
+            Rect scrollArea = new Rect(0F, 0F, w, scrollHeight);
+
+            Widgets.BeginScrollView(outRect, ref scrollPosition, scrollArea);
+            
+            if (0 < playerMaps.Count)
+                DrawMapList(playerMaps, scrollArea);
+
+            Widgets.EndScrollView();
+
+            if (selectedMap == null && 0 < playerMaps.Count)
+                selectedMap = playerMaps[0];
+
+            float buttonWidth = 160F;
+            float buttonHeight = 40F;
+
+            Rect buttonArea = new Rect(0f, rect.yMax - 70f, rect.width, 70f);
+		    Rect acceptButtonArea = new Rect(buttonArea.xMax - buttonWidth, buttonArea.yMax - buttonHeight, buttonWidth, buttonHeight);
+
+            if (Widgets.ButtonText(acceptButtonArea, "AcceptButton".Translate()))
+            {
+                if (AcceptanceReport.Accepted && selectedMap != null)
+                {
+                    Find.WindowStack.Add(Dialog_MessageBox.CreateConfirmation($"{selectedMap.Parent.Label} will be sold, and its contents will become inaccessible. Anything within other settlements or caravans will be untouched.", FinalizeDecision, false, "ConfirmDecisionsTitle".Translate()));
+                }
+                else
+                {
+                    Messages.Message(AcceptanceReport.Reason, MessageTypeDefOf.RejectInput, historical: false);
+                }
+            }
+
+            // Hide cancel button for the final part so you can't just cancel to find the archonexus for free
+            if (!isFinal)
+            {
+                Rect cancelButtonArea = acceptButtonArea;
+                cancelButtonArea.x = buttonArea.x;
+                if (Widgets.ButtonText(cancelButtonArea, "Cancel".Translate()))
+                {
+                    if (cancel != null)
+                    {
+                        cancel();
+                    }
+                    Close();
+                }
+            }
+        }
+
+        private bool ValidateSelection()
+        {
+            Log.Message($"Wealth of selected settlement: {selectedMap.wealthWatcher.WealthTotal}");
+            return QuestLimits.WealthRequired <= selectedMap.wealthWatcher.WealthTotal;
+        }
+
+        private void FinalizeDecision()
+        {
+            Close();
+            postSelected(selectedMap);
+        }
+
+        private void DrawMapList(List<Map> maps, Rect rect)
+        {
+            Text.Anchor = TextAnchor.UpperLeft;
+            int count = maps.Count;
+            float startY = rect.y + 30f;
+            for (int i = 0; i < count; i++)
+            {
+                Rect rowArea = new Rect(rect.x, startY + (float) i * 30f, rect.width, 30f);
+                Map map = maps[i];
+
+                if (i % 2 == 1)
+                    Widgets.DrawLightHighlight(rowArea);
+
+                GUI.BeginGroup(rowArea);
+
+                // Display hover effects
+                Rect tooltipArea = new Rect(0F, 0F, 80F + 196F, rowArea.height);
+                if (Mouse.IsOver(tooltipArea))
+                {
+                    Widgets.DrawHighlight(tooltipArea);
+                }
+
+                // Add info button
+                Widgets.InfoCardButton(0F, 0F, map.Parent);
+
+                // Add settlement label
+                Text.Anchor = TextAnchor.MiddleLeft;
+                Text.WordWrap = false;
+                Widgets.Label(new Rect(40F, 0F, 196F, rowArea.height), map.Parent.Label);
+                Text.WordWrap = true;
+
+                // Add highlighting for button area
+                Rect buttonLargeArea = new Rect(tooltipArea.x + tooltipArea.width, 0F, rowArea.width - tooltipArea.width, tooltipArea.height);
+                if (Mouse.IsOver(buttonLargeArea))
+                    Widgets.DrawHighlight(buttonLargeArea);
+
+                // Add radio button
+                float dim = rowArea.height;
+                Rect buttonArea = new Rect(rowArea.width - dim, 3F, dim, dim);
+                if (Widgets.RadioButton(buttonArea.x, buttonArea.y, map == selectedMap))
+                {
+                    selectedMap = map;
+                    SoundDefOf.Checkbox_TurnedOn.PlayOneShotOnCamera();
+                }
+
+                GenUI.ResetLabelAlign();
+                GUI.EndGroup();
+            }
+        }
+    }
+}
---
+++ Source/1.5/QuestParts.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using RimWorld.QuestGen;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+
+namespace AlternateArchonexus
+{
+    public class QuestPart_SubquestGenerator_AlternateArchonexusVictory : QuestPart_SubquestGenerator
+    {
+        public Faction civilOutlander, empire, roughTribe, roughOutlander;
+
+        protected override Slate InitSlate()
+        {
+            Slate slate = new Slate();
+            slate.Set("civilOutlander", civilOutlander);
+            slate.Set("empire", empire);
+            slate.Set("roughTribe", roughTribe);
+            slate.Set("roughOutlander", roughOutlander);
+            return slate;
+        }
+
+        protected override QuestScriptDef GetNextSubquestDef()
+        {
+            int index = quest.GetSubquests(QuestState.EndedSuccess).Count() % subquestDefs.Count;
+            QuestScriptDef questScriptDef = subquestDefs[index];
+            if (!questScriptDef.CanRun(InitSlate()))
+            {
+                return null;
+            }
+            return questScriptDef;
+        }
+
+        public override void Notify_FactionRemoved(Faction faction)
+        {
+            if (civilOutlander == faction)
+                civilOutlander = null;
+            else if (empire == faction)
+                empire = null;
+            else if (roughTribe == faction)
+                roughTribe = null;
+            else if (roughOutlander == faction)
+                roughOutlander = null;
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_References.Look(ref civilOutlander, "civilOutlander");
+            Scribe_References.Look(ref empire, "empire");
+            Scribe_References.Look(ref roughTribe, "roughTribe");
+            Scribe_References.Look(ref roughOutlander, "roughOutlander");
+        }
+    }
+
+    public class QuestPart_AlternateNewColony : QuestPart
+    {
+        public string inSignal, outSignalCompleted, outSignalCancelled;
+
+        public Faction otherFaction;
+        public WorldObjectDef worldObjectDef;
+
+        public bool isFinal;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref inSignal, "inSignal");
+            Scribe_References.Look(ref otherFaction, "otherFaction");
+            Scribe_Values.Look(ref outSignalCompleted, "outSignalCompleted");
+            Scribe_Values.Look(ref outSignalCancelled, "outSignalCancelled");
+            Scribe_Defs.Look(ref worldObjectDef, "worldObjecctDef");
+        }
+
+        public override void Notify_QuestSignalReceived(Signal signal)
+        {
+            Log.Message("[AX] Signal Received");
+            base.Notify_QuestSignalReceived(signal);
+
+            Log.Message($"[AX] {inSignal} : {signal.tag}");
+
+            if (signal.tag != inSignal)
+                return;
+
+            Log.Message("[AX] Processing Signal");
+
+            Find.MainTabsRoot.EscapeCurrentTab(playSound: false);
+            Find.World.renderer.RegenerateLayersIfDirtyInLongEvent(); // ?
+
+            // Find all maps that belong to the player
+            List<Map> playerMaps = new List<Map>();
+
+            foreach (Map map in Find.Maps)
+            {
+                if (map.IsPlayerHome)
+                    playerMaps.Add(map);
+            }
+
+            // Display dialog to select map
+            Find.WindowStack.Add(new Dialog_ChooseSoldSettlement(PostSellSelection, delegate
+            {
+                if (!outSignalCancelled.NullOrEmpty())
+                    Find.SignalManager.SendSignal(new Signal(outSignalCancelled));
+            }){isFinal = isFinal});
+        }
+
+        protected virtual void PostSellSelection(Map map)
+        {
+            Find.WindowStack.Add(new Screen_ArchonexusSettlementCinematics(delegate
+            {
+                CameraJumper.TryJump(CameraJumper.GetWorldTargetOfMap(map));
+            }, delegate
+            {
+                if (!isFinal)
+                {
+                    MoveColonyUtility.PickNewColonyTile(TileChosen, delegate
+                    {
+                        if (!outSignalCancelled.NullOrEmpty())
+                        {
+                            Find.SignalManager.SendSignal(new Signal(outSignalCancelled));
+                        }
+                    });
+                }
+                
+                SellSettlement(map);
+                ScreenFader.StartFade(Color.clear, 2f);
+            }));
+        }
+
+        protected virtual void TileChosen(int chosenTile)
+        {
+            LongEventHandler.QueueLongEvent(delegate
+            {
+                Find.MusicManagerPlay.ForceFadeoutAndSilenceFor(120f);
+
+                WorldObjectDef worldObjectClarified = worldObjectDef ?? WorldObjectDefOf.Settlement;
+
+                // Establish the new settlement
+                Settlement settlement = WorldObjectMaker.MakeWorldObject(worldObjectClarified) as Settlement;
+                settlement.SetFaction(Faction.OfPlayer);
+                settlement.Tile = chosenTile;
+                settlement.Name = SettlementNameGenerator.GenerateSettlementName(settlement, Faction.OfPlayer.def.playerInitialSettlementNameMaker);
+                Find.WorldObjects.Add(settlement);
+                Map orGenerateMap = GetOrGenerateMapUtility.GetOrGenerateMap(settlement.Tile, worldObjectClarified);
+
+                CameraJumper.TryJump(MapGenerator.PlayerStartSpot, settlement.Map);
+                Find.TickManager.CurTimeSpeed = TimeSpeed.Normal;
+                if (!outSignalCompleted.NullOrEmpty())
+                {
+                    Find.SignalManager.SendSignal(new Signal(outSignalCompleted));
+                }
+            }, "GeneratingMap", doAsynchronously: false, null);
+        }
+
+        protected void SellSettlement(Map map)
+        {
+            // End quests that should probably end at this point
+            foreach (Quest quest in Find.QuestManager.QuestsListForReading)
+            {
+                if (quest.IsEndOnNewArchonexusSettlement())
+                {
+                    quest.hidden = true;
+                    quest.End(QuestEndOutcome.Unknown, sendLetter: false);
+                }
+            }
+            
+            List<Pawn> pawnsToRemove = new List<Pawn>();
+
+            foreach (Pawn pawn in map.mapPawns.SpawnedColonyAnimals)
+                pawnsToRemove.Add(pawn);
+
+            foreach (Pawn pawn in map.mapPawns.AllPawns)
+            {
+                // Clear colonists and prisoners that are not within any caravans
+                if ((pawn.IsColonist || pawn.IsPrisonerOfColony) && !pawn.IsCaravanMember())
+                    pawnsToRemove.Add(pawn);
+            }
+
+            foreach (Pawn pawn in pawnsToRemove)
+            {
+                if (pawn.Spawned)
+                    pawn.DeSpawn();
+                    
+                if (!pawn.IsWorldPawn())
+                    Find.WorldPawns.PassToWorld(pawn);
+            }
+            
+            map.Parent.SetFaction(null);
+            Current.Game.DeinitAndRemoveMap(map, false);
+            map.Parent.Destroy();
+
+            if (otherFaction != null)
+                SettleUtility.AddNewHome(map.Tile, otherFaction);
+        }
+    }
+}
